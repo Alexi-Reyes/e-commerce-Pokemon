3210086b22068459cbeb78a025d3fcc167fe24ee
Alexi Reyes <alexireyes6796@gmail.com>
2024-01-12 16:26:31 +0100

    added pokemonBasket localstorage

--- main.js
+++ main.js
@@ -9,18 +9,55 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`)
       fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         .then((response) => response.json())
         .then((pokeData) => {
+            let pokemonTypeArray = [];
+
             const productDiv = document.createElement('div');
             productDiv.className = `product`;
             pokeData.types.forEach(element => {
                 productDiv.className += ` ${element.type.name}`
+                pokemonTypeArray.push(element.type.name)
             });
             document.querySelector(".products-grid").appendChild(productDiv);
 
             document.querySelectorAll(".product")[pokemonIndex].innerHTML += `${pokeData.name}`
 
-            const imgElement = document.createElement('img');
-            imgElement.src = `${pokeData.sprites.front_default}`;
-            document.querySelectorAll(".product")[pokemonIndex].appendChild(imgElement);
+            const productImg = document.createElement('img');
+            productImg.src = `${pokeData.sprites.front_default}`;
+            document.querySelectorAll(".product")[pokemonIndex].appendChild(productImg);
+
+
+            pokeData.types.forEach(element => {
+                const pokemonTypes = document.createElement("p")
+                pokemonTypes.textContent = element.type.name;
+                pokemonTypes.className = `type-tag ${element.type.name}`;
+                document.querySelectorAll(".product")[pokemonIndex].appendChild(pokemonTypes);
+            });
+
+            const productBtn = document.createElement('button');
+            productBtn.textContent = "Add to basket";
+            productBtn.className = "product-btn";
+
+            console.log(pokemonTypeArray)
+            const pokemonInfo = {
+                "name": pokeData.name,
+                "type": pokemonTypeArray,
+                "imageURL": pokeData.sprites.front_default,
+                "level": 10
+            };
+
+
+
+            productBtn.addEventListener('click', () => {
+                if (!localStorage.getItem('pokemonBasket')) {
+                    localStorage.setItem('pokemonBasket', JSON.stringify(pokemonInfo));
+                } else {
+                    newBasket = localStorage.getItem('pokemonBasket')
+                    newBasket += JSON.stringify(pokemonInfo)
+                    localStorage.setItem('pokemonBasket', newBasket);
+                }
+            });
+            document.querySelectorAll(".product")[pokemonIndex].appendChild(productBtn);
+
             
             pokemonIndex++;
         })
@@ -51,6 +88,8 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch((error) => document.querySelector(".products-grid").innerHTML = `No pokemon found`)
     });
 });
+
+// type filter 
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#typeFilter").addEventListener('change', (event) => {
